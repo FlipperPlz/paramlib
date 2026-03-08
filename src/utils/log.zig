bbb780c4c504e1093108967d6a6a7803ef2210cf
Ellie <flipperplz@protonmail.com>
2026-03-08 19:06:29 -0400

    fix(zig-0.16): Fix github merge issue

--- src/utils/log.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-pub const enable_logging = true;
-
-pub fn debug(comptime fmt: []const u8, args: anytype) void {
-    if (enable_logging) {
-        std.debug.print(fmt ++ "\n", args);
-    }
-}
