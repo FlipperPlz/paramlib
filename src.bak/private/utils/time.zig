6b7711fce564d44b4a0a63ddf2ad1ffc1254eeec
Ellie Cyr <flipperplz@protonmail.com>
2026-03-26 09:51:53 -0400

    feat(rewrite): Remove Old Codebase

--- src.bak/private/utils/time.zig
+++
@@ -1,9 +0,0 @@
-pub const std = @import("std");
-
-pub fn getTimeMs(io: std.Io, clock: std.Io.Clock) i64 {
-    const timestamp: std.Io.Timestamp = clock.now(io) catch {
-        @panic("unsupported clock");
-    };
-
-    return timestamp.toMilliseconds();
-}
--- src.bak/root.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-const testing = std.testing;
-
-test {
-    testing.refAllDecls(@import("utils/strings.zig"));
-    testing.refAllDecls(@import("utils/memory.zig"));
-}
